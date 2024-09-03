0efd119b357c591975a27e71b540d5b125708cb2
erik-martins <erik.ozzetti.martins@gmail.com>
2024-09-03 17:50:21 -0300

    feat: loaging logo  google

---
+++ src/shared/components/loading/Loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-32 h-32 border-t-2 border-b-2 rounded-full animate-spin border-primary"></div>
+    </div>
+  );
+};
