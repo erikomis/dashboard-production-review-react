cf9793fca94bd92301c3443d4cde25d5a0070361
erik-martins <erik.ozzetti.martins@gmail.com>
2024-09-13 19:51:49 -0300

    fix: me service and service

--- src/shared/services/me.ts
+++ src/shared/services/me.ts
@@ -1,17 +1,34 @@
-
-import { user } from "../../context/authContext";
 import { api } from "./api";
 
+type role = {
+  id: number;
+  name: string;
+  permissions: [
+    {
+      id: number;
+      name: string;
+    }
+  ];
+};
+
+type user = {
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+  roles: role[];
+  active: boolean;
+};
 export const me = async () => {
   try {
     const response = await api.request<user>({
       url: "/user/me",
-      method: "GET",  
+      method: "GET",
       withCredentials: true,
     });
     return response.data;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error:any) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
     if (error instanceof Error) {
       throw error;
     }
