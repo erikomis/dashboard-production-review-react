c0cb984dab1c4b505c3399f89803d4b72f645442
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-12 14:46:46 -0300

    feat:  component select

--- src/shared/components/select.tsx
+++ src/shared/components/select.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { cn } from "../utils/utils";
 import { tv } from "tailwind-variants";
 
@@ -30,19 +30,6 @@ const selectVariants = tv({
   //   },
 });
 
-interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
-  children?: React.ReactNode;
-  Label?: React.ReactNode;
-  color?:
-    | "default"
-    | "destructive"
-    | "outline"
-    | "secondary"
-    | "ghost"
-    | "link";
-  sizeType?: "default" | "sm" | "xs" | "lg" | "icon";
-}
-
 // export const Select = ({ children, color, sizeType, ...rest }: SelectProps) => {
 //   return (
 //     <select
@@ -54,59 +41,141 @@ interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
 //   );
 // };
 
-export const SelectGroupOne = ({ Label, children, ...rest }: SelectProps) => {
-  const [selectedOption, setSelectedOption] = useState<string>("");
-  const [isOptionSelected, setIsOptionSelected] = useState<boolean>(false);
+// interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+//   children?: React.ReactNode;
+//   Label?: React.ReactNode;
+//   color?:
+//     | "default"
+//     | "destructive"
+//     | "outline"
+//     | "secondary"
+//     | "ghost"
+//     | "link";
+//   sizeType?: "default" | "sm" | "xs" | "lg" | "icon";
+// }
 
-  const changeTextColor = () => {
-    setIsOptionSelected(true);
-  };
+// export const SelectGroupOne = React.forwardRef< SelectProps>({ Label, children, ...rest }) => {
+//   const [selectedOption, setSelectedOption] = useState<string>("");
+//   const [isOptionSelected, setIsOptionSelected] = useState<boolean>(false);
 
-  return (
-    <div className="mb-4.5">
-      {/* <label className="mb-2.5 block text-black dark:text-white">Subject</label> */}
-      {Label}
-      <div className="relative z-20 bg-transparent dark:bg-form-input">
-        <select
-          {...rest}
-          value={selectedOption}
-          onChange={(e) => {
-            setSelectedOption(e.target.value);
-            changeTextColor();
-          }}
-          // className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary ${
+//   const changeTextColor = () => {
+//     setIsOptionSelected(true);
+//   };
 
-          // }`}
+//   return (
+//     <div className="mb-4.5">
+//       {/* <label className="mb-2.5 block text-black dark:text-white">Subject</label> */}
+//       {Label}
+//       <div className="relative z-20 bg-transparent dark:bg-form-input">
+//         <select
+//           {...rest}
+//           value={selectedOption}
+//           onChange={(e) => {
+//             setSelectedOption(e.target.value);
+//             changeTextColor();
+//           }}
+//           // className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary ${
 
-          className={cn(
-            selectVariants(),
-            rest.className,
-            isOptionSelected ? "text-black dark:text-white" : ""
-          )}
-        >
-         {children}
-        </select>
+//           // }`}
 
-        <span className="absolute z-30 -translate-y-1/2 top-1/2 right-4">
-          <svg
-            className="fill-current"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+//           className={cn(
+//             selectVariants(),
+//             rest.className,
+//             isOptionSelected ? "text-black dark:text-white" : ""
+//           )}
+//         >
+//          {children}
+//         </select>
+
+//         <span className="absolute z-30 -translate-y-1/2 top-1/2 right-4">
+//           <svg
+//             className="fill-current"
+//             width="24"
+//             height="24"
+//             viewBox="0 0 24 24"
+//             fill="none"
+//             xmlns="http://www.w3.org/2000/svg"
+//           >
+//             <g opacity="0.8">
+//               <path
+//                 fillRule="evenodd"
+//                 clipRule="evenodd"
+//                 d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+//                 fill=""
+//               ></path>
+//             </g>
+//           </svg>
+//         </span>
+//       </div>
+//     </div>
+//   );
+// };
+
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  children?: React.ReactNode;
+  Label?: React.ReactNode;
+  color?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  sizeType?: "default" | "sm" | "xs" | "lg" | "icon";
+  error?: string;
+}
+
+export const SelectGroupOne = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ Label, children, ...rest }, ref) => {
+    const [isOptionSelected, setIsOptionSelected] = useState<boolean>(false);
+    const handleChange = () => {
+      setIsOptionSelected(true);
+    };
+
+    return (
+      <div className="mb-4.5">
+        {Label}
+        <div className="relative z-20 bg-transparent dark:bg-form-input">
+          <select
+            {...rest}
+            onChange={handleChange}
+            className={cn(
+              selectVariants(),
+              "relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary",
+              isOptionSelected ? "text-black dark:text-white" : "",
+              rest.className
+            )}
+            ref={ref}
           >
-            <g opacity="0.8">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                fill=""
-              ></path>
-            </g>
-          </svg>
-        </span>
+            {children}
+          </select>
+
+          {rest.error && (
+            <span className="my-2 text-xs font-bold t text-danger">
+              {rest.error}
+            </span>
+          )}
+          <span className="absolute z-30 -translate-y-1/2 top-1/2 right-4">
+            <svg
+              className="fill-current"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g opacity="0.8">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                  fill=""
+                ></path>
+              </g>
+            </svg>
+          </span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
