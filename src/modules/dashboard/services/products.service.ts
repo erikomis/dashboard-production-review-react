23bb7f459cff65781e12e8206dc3f567a26b1cf3
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-13 19:47:09 -0300

    feat:  types request reponse

--- src/modules/dashboard/services/products.service.ts
+++ src/modules/dashboard/services/products.service.ts
@@ -1,17 +1,78 @@
 import { AxiosError } from "axios";
 import { api } from "../../../shared/services/api";
 
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  slug: string;
+};
+
+interface PageInfo {
+  size: number;
+  number: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+type PaginatedProductsResponse = {
+  content: Product[];
+  page: PageInfo;
+};
+
+type CreateProduct = Omit<Product, "id">;
 export const ProductsService = {
   fetchProducts: async () => {
     try {
-      const response = await api.request({
+      const response = await api.request<PaginatedProductsResponse>({
         method: "GET",
         url: "/production/list?page=0&size=10",
       });
-      if (response.status !== 200 || response.data.status !== 201) {
-        throw Error(response.data.message);
-      }
       return response.data;
+    } catch (er) {
+      const error = er as AxiosError<{ message: string }>;
+      const message =
+        (error.response?.data?.message as string) || error.message;
+      throw new Error(`${message}`);
+    }
+  },
+  fechProductCreate: async (data: CreateProduct) => {
+    try {
+      const response = await api.request({
+        method: "POST",
+        url: "/production/",
+        data,
+      });
+      return response;
+    } catch (er) {
+      const error = er as AxiosError<{ message: string }>;
+      const message =
+        (error.response?.data?.message as string) || error.message;
+      throw new Error(`${message}`);
+    }
+  },
+  fetchProductUpdate: async (data: Product) => {
+    try {
+      const response = await api.request({
+        method: "PUT",
+        url: "/production/" + data.id,
+        data,
+      });
+      return response;
+    } catch (er) {
+      const error = er as AxiosError<{ message: string }>;
+      const message =
+        (error.response?.data?.message as string) || error.message;
+      throw new Error(`${message}`);
+    }
+  },
+  fetchProductDelete: async (id: number) => {
+    try {
+      const response = await api.request({
+        method: "DELETE",
+        url: "/production/" + id,
+      });
+      return response;
     } catch (er) {
       const error = er as AxiosError<{ message: string }>;
       const message =
